e1d3658630985ce956c98605103d55278cfdb114
123usef <yousifm836@gmail>
2025-12-18 12:39:28 +0400

    Add project files.

---
+++ PLL.MVC.OmanDigitalShop/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PLL.MVC.OmanDigitalShop.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
